dcbc313183283697f2b619b16c13374752848315
ben <ben.franklin@hidglobal.com>
2021-04-12 08:52:52 +0530

    request replaced

--- utils/weaterDetail.js
+++ utils/weaterDetail.js
@@ -1,25 +1,22 @@
 const chalk = require('chalk');
-const fetch =require('fetch');
-
+const request=require('request')
 
 const weatherDetail=(lat,long,callback)=>{
     const url=`http://api.weatherstack.com/current?access_key=787dfccb498213e4f7351be5a27d122d&query=${lat},${long}`
-    fetch(url).then((res)=>{
-        res.json().then(
-            (data)=>{
-                if (data.body.error){
-                    callback(JSON.stringify(res.body.error.info) ,undefined) 
-                }else{
-                    const data=data.body.current;
-                    callback(undefined, "It is "+data.weather_descriptions[0]+" but the temperature is "+chalk.green(data.temperature) +' and it feels like '+data.feelslike
-                    )
-                }
-            }
-        )
-    }).catch((err)=>{ 
-         callback("Unable to connect",undefined)
+    request({url,json:true},
+    (err,res)=>{
+        if(err){
+            callback("Unable to connect",undefined)
+        }else if (res.body.error){
+            callback(JSON.stringify(res.body.error.info) ,undefined) 
+        }else{
+            const data=res.body.current;
+            callback(undefined, "It is "+data.weather_descriptions[0]+" but the temperature is "+ data.temperature +' and it feels like '+data.feelslike
+            )
+          
+        }}
+    ) 
 }
 
-    )
-}
+
 module.exports=weatherDetail
